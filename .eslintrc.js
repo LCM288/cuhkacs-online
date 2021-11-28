20ef585af5fb42b624d7faba1524b440d260e85f
LCM288 <charlielicmli@gmail.com>
2021-11-28 19:40:56 +0800

    some settings

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["react", "@typescript-eslint", "jest", "import"],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "esnext",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  ignorePatterns: ["**/*.generated.ts"],
+  rules: {
+    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "never",
+      },
+    ],
+    "comma-spacing": "error",
+    "comma-style": "error",
+    "react/jsx-props-no-spreading": "off",
+    "import/extensions": "off",
+  },
+};
