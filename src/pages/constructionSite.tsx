0b48b033e7c3c6c068a114834b29d6d6e56e0a3c
LCM288 <charlielicmli@gmail.com>
2021-12-21 21:33:43 +0800

    enable tunnel to real page on cuacs.org

--- src/pages/constructionSite.tsx
+++ src/pages/constructionSite.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Hero, Heading, Icon } from "react-bulma-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWrench } from "@fortawesome/free-solid-svg-icons";
+import App from "app";
 
 const ConstructionSite = (): React.ReactElement => {
+  const [count, increase] = useReducer((state) => state + 1, 0);
+  if (count >= 20) {
+    return <App />;
+  }
   return (
     <Hero size="fullheight" color="warning">
       <Hero.Body>
         <Heading style={{ margin: "auto" }}>
-          This site is under construction
-          <Icon>
-            <FontAwesomeIcon icon={faWrench} />
-          </Icon>
+          <div className="icon-text">
+            This site is under construction
+            <Icon className="ml-2" onClick={increase}>
+              <FontAwesomeIcon icon={faWrench} />
+            </Icon>
+          </div>
         </Heading>
       </Hero.Body>
     </Hero>
