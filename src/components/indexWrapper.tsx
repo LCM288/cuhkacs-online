a2b05d655304f0570961e006ab45860886fa02fe
LCM288 <charlielicmli@gmail.com>
2021-12-22 18:30:26 +0800

    fix position issues

--- src/components/indexWrapper.tsx
+++ src/components/indexWrapper.tsx
@@ -6,26 +6,17 @@ interface Props {
 }
 
 const IndexWrapper = ({ children }: Props): React.ReactElement => (
-  <div
-    style={{
-      top: "50%",
-      left: "50%",
-      transform: "translate(-50%, max(-50%, -46.75vh))",
-      position: "absolute",
-    }}
-  >
-    <Section>
-      <Container
-        className="has-text-centered"
-        style={{
-          minWidth: "75vw",
-          borderRadius: ".25rem",
-        }}
-      >
-        {children}
-      </Container>
-    </Section>
-  </div>
+  <Section className="pt-0">
+    <Container
+      className="has-text-centered"
+      style={{
+        minWidth: "75vw",
+        borderRadius: ".25rem",
+      }}
+    >
+      {children}
+    </Container>
+  </Section>
 );
 
 export default IndexWrapper;
